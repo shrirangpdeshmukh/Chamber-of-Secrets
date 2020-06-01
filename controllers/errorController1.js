59b8b185bd4861602b32f2683b7ef543345f04a2
Shrirang <deshmukhshrirang01@gmail.com>
2020-06-01 10:39:52 +0530

    Just A little Additon

--- controllers/errorController1.js
+++ controllers/errorController1.js
@@ -1,11 +1,13 @@
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
-
+  //console.log(err.stack);
+  console.log(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
+    error: err,
   });
 };
